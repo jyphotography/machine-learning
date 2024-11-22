174f34c437667b873e9572cf68d7e0d4aae1b2d8
Jason <hiremingjiey@gmail.com>
2024-11-22 20:06:07 +0000

    docker with expose9696

--- midterm/src/test.py
+++ midterm/src/test.py
@@ -1,7 +1,7 @@
 import requests
 import json
 test_json = {"BEDS": 4, "BATH": 2, "PROPERTYSQFT": 2184, "SUBLOCALITY": "Queens County"}
-url = 'http://127.0.0.1:5000/predict'
+url = 'http://0.0.0.0:9696/predict'
 response = requests.post(url, json=test_json)
 result = response.json()
 
